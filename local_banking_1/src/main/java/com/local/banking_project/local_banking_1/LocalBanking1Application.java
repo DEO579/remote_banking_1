3fe763d2f200f2f45988bd4f64c18fdb716b78ae
DEO579 <deo.kr59998@gmail.com>
2024-08-04 22:17:52 +0530

    avanger :add war file feature

--- local_banking_1/src/main/java/com/local/banking_project/local_banking_1/LocalBanking1Application.java
+++ local_banking_1/src/main/java/com/local/banking_project/local_banking_1/LocalBanking1Application.java
@@ -2,9 +2,10 @@ package com.local.banking_project.local_banking_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LocalBanking1Application {
+public class LocalBanking1Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LocalBanking1Application.class, args);
